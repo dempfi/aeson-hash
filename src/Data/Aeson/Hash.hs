7105799d73a0a0d892938ed4e9d701d783312c4a
Ike Ku <mail@dempfi.com>
2019-03-09 14:37:52 +0200

    Use GHC 8.6.4 and reformat sourcecode

--- src/Data/Aeson/Hash.hs
+++ src/Data/Aeson/Hash.hs
@@ -1,18 +1,22 @@
 module Data.Aeson.Hash
-  (
-    hash
-  ) where
+  ( hash
+  )
+where
 
-import Data.List (sort)
-import Data.Aeson (ToJSON, toJSON)
-import qualified Data.Text as Text
-import qualified Data.Vector as Vector
-import qualified Data.ByteString as BS
-import qualified Data.Aeson.Types as Types
-import qualified Crypto.Hash.SHA256 as SHA256
-import Data.Scientific (Scientific, toRealFloat)
-import Data.HashMap.Strict (toList)
-import Data.ByteString.Char8 (pack)
+import           Data.List                      ( sort )
+import           Data.Aeson                     ( ToJSON
+                                                , toJSON
+                                                )
+import qualified Data.Text                     as Text
+import qualified Data.Vector                   as Vector
+import qualified Data.ByteString               as BS
+import qualified Data.Aeson.Types              as Types
+import qualified Crypto.Hash.SHA256            as SHA256
+import           Data.Scientific                ( Scientific
+                                                , toRealFloat
+                                                )
+import           Data.HashMap.Strict            ( toList )
+import           Data.ByteString.Char8          ( pack )
 
 sha256 :: String -> BS.ByteString -> BS.ByteString
 sha256 prefix bs = SHA256.hash $ BS.append (pack prefix) bs
@@ -24,59 +28,54 @@ hashNull :: BS.ByteString
 hashNull = BS.empty
 
 hashBool :: Bool -> BS.ByteString
-hashBool b = sha256 "b" $ pack bs
-  where
-    bs = if b then "1" else "0"
+hashBool b = sha256 "b" $ pack bs where bs = if b then "1" else "0"
 
 significandString :: Double -> String
-significandString d
-  | d == 0    = ""
-  | d >= 1    = "1" ++ significandString ((d - 1) * 2)
-  | otherwise = "0" ++ significandString (d * 2)
+significandString d | d == 0    = ""
+                    | d >= 1    = "1" ++ significandString ((d - 1) * 2)
+                    | otherwise = "0" ++ significandString (d * 2)
 
 -- Specific to object hash, wrong (exp 1 = 0) exponent
 customExponent :: Double -> Int -> Int
-customExponent m n
-  | m > 1     = customExponent (m / 2) (n + 1)
-  | m < 0.5   = customExponent (m * 2) (n - 1)
-  | otherwise = n
+customExponent m n | m > 1     = customExponent (m / 2) (n + 1)
+                   | m < 0.5   = customExponent (m * 2) (n - 1)
+                   | otherwise = n
 
 -- Specific to object hash, not mathematically accurate
 customSignificand :: Double -> Double
-customSignificand m
-  | m > 1     = customSignificand (m / 2)
-  | m < 0.5   = customSignificand (m * 2)
-  | otherwise = m
+customSignificand m | m > 1     = customSignificand (m / 2)
+                    | m < 0.5   = customSignificand (m * 2)
+                    | otherwise = m
 
 normalizeDouble :: Double -> String
-normalizeDouble d
-  | isInfinite d = if d > 0 then "Infinity" else "-Infinity"
-  | isNaN d      = "NaN"
-  | d == 0       = "+0:"
-  | d < 0        = "-" ++ stringified (-d)
-  | d > 0        = "+" ++ stringified d
-  where
-    stringified n =
-      show (customExponent n 0) ++ ":" ++ significandString (customSignificand n)
+normalizeDouble d | isInfinite d = if d > 0 then "Infinity" else "-Infinity"
+                  | isNaN d      = "NaN"
+                  | d == 0       = "+0:"
+                  | d < 0        = "-" ++ stringified (-d)
+                  | d > 0        = "+" ++ stringified d
+                  | otherwise    = "NaN"
+ where
+  stringified n =
+    show (customExponent n 0) ++ ":" ++ significandString (customSignificand n)
 
 hashNumber :: Scientific -> BS.ByteString
 hashNumber n = sha256 "f" $ pack $ normalizeDouble $ toRealFloat n
 
 hashArray :: Types.Array -> BS.ByteString
-hashArray arr = sha256 "l" $ BS.concat $ bsList
-  where
-    bsList = Vector.toList $ Vector.map hashValue arr
+hashArray arr = sha256 "l" $ BS.concat bsList
+  where bsList = Vector.toList $ Vector.map hashValue arr
 
 hashObject :: Types.Object -> BS.ByteString
 hashObject obj = sha256 "d" $ BS.concat $ sort hashedElements
-  where
-    hashElement (k, v) = BS.append (hashString k) (hashValue v)
-    hashedElements = map hashElement $ toList obj
+ where
+  hashElement (k, v) = BS.append (hashString k) (hashValue v)
+
+  hashedElements = map hashElement $ toList obj
 
 hashValue :: Types.Value -> BS.ByteString
-hashValue Types.Null = hashNull
-hashValue (Types.Bool b) = hashBool b
-hashValue (Types.Array a) = hashArray a
+hashValue Types.Null       = hashNull
+hashValue (Types.Bool   b) = hashBool b
+hashValue (Types.Array  a) = hashArray a
 hashValue (Types.String s) = hashString s
 hashValue (Types.Number n) = hashNumber n
 hashValue (Types.Object o) = hashObject o
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,73 +1,90 @@
 {-# LANGUAGE DeriveGeneric, DuplicateRecordFields #-}
 
-import Data.Aeson
-import GHC.Generics
-import Test.Hspec
-import Text.Printf (printf)
-import Data.Aeson.Hash (hash)
-import qualified Data.ByteString as BS
+import           Data.Aeson
+import           GHC.Generics
+import           Test.Hspec
+import           Text.Printf                    ( printf )
+import           Data.Aeson.Hash                ( hash )
+import qualified Data.ByteString               as BS
 
 h :: ToJSON a => a -> String
 h = concatMap (printf "%02x") . BS.unpack . hash
 
-data Doubles = Doubles [Double] deriving (Generic, Show)
-instance ToJSON Doubles where toEncoding = genericToEncoding defaultOptions
+newtype Doubles = Doubles [Double] deriving (Generic, Show)
+instance ToJSON Doubles where
+  toEncoding = genericToEncoding defaultOptions
 
-data Strings = Strings [String] deriving (Generic, Show)
-instance ToJSON Strings where toEncoding = genericToEncoding defaultOptions
+newtype Strings = Strings [String] deriving (Generic, Show)
+instance ToJSON Strings where
+  toEncoding = genericToEncoding defaultOptions
 
-data Ints = Ints [Int] deriving (Generic, Show)
-instance ToJSON Ints where toEncoding = genericToEncoding defaultOptions
+newtype Ints = Ints [Int] deriving (Generic, Show)
+instance ToJSON Ints where
+  toEncoding = genericToEncoding defaultOptions
 
-data Booleans = Booleans [Bool] deriving (Generic, Show)
-instance ToJSON Booleans where toEncoding = genericToEncoding defaultOptions
+newtype Booleans = Booleans [Bool] deriving (Generic, Show)
+instance ToJSON Booleans where
+  toEncoding = genericToEncoding defaultOptions
 
-data Obj1 = Obj1 { foo :: String } deriving (Generic, Show)
-instance ToJSON Obj1 where toEncoding = genericToEncoding defaultOptions
+newtype Obj1 = Obj1 { foo :: String } deriving (Generic, Show)
+instance ToJSON Obj1 where
+  toEncoding = genericToEncoding defaultOptions
 
 data Obj2 = Obj2 { foo :: [String], qux :: [String] } deriving (Generic, Show)
-instance ToJSON Obj2 where toEncoding = genericToEncoding defaultOptions
+instance ToJSON Obj2 where
+  toEncoding = genericToEncoding defaultOptions
 
 main :: IO ()
-main = hspec $ do
-  describe "Data.Aeson.Hash" $ do
-    context "List" $ do
-      context "Of strings" $ do
-        specify "Empty" $ do
-          h (Strings []) `shouldBe` "acac86c0e609ca906f632b0e2dacccb2b77d22b0621f20ebece1a4835b93f6f0"
+main = hspec $ describe "Data.Aeson.Hash" $ do
+  context "List" $ do
+    context "Of strings" $ do
+      specify "Empty"
+        $ h (Strings [])
+        `shouldBe` "acac86c0e609ca906f632b0e2dacccb2b77d22b0621f20ebece1a4835b93f6f0"
 
-        specify "Singleton" $ do
-          h (Strings ["foo"]) `shouldBe` "268bc27d4974d9d576222e4cdbb8f7c6bd6791894098645a19eeca9c102d0964"
+      specify "Singleton"
+        $ h (Strings ["foo"])
+        `shouldBe` "268bc27d4974d9d576222e4cdbb8f7c6bd6791894098645a19eeca9c102d0964"
 
-        specify "Simple" $ do
-          h (Strings ["foo", "bar"]) `shouldBe` "32ae896c413cfdc79eec68be9139c86ded8b279238467c216cf2bec4d5f1e4a2"
+      specify "Simple"
+        $ h (Strings ["foo", "bar"])
+        `shouldBe` "32ae896c413cfdc79eec68be9139c86ded8b279238467c216cf2bec4d5f1e4a2"
 
-      context "Of ints" $ do
-        specify "Singleton" $ do
-          h (Ints [123]) `shouldBe` "2e72db006266ed9cdaa353aa22b9213e8a3c69c838349437c06896b1b34cee36"
+    context "Of ints" $ do
+      specify "Singleton"
+        $ h (Ints [123])
+        `shouldBe` "2e72db006266ed9cdaa353aa22b9213e8a3c69c838349437c06896b1b34cee36"
 
-        specify "Simple" $ do
-          h (Ints [0, 1, 2, 3]) `shouldBe` "9c6a042b7dfd0819735a839409456b303fff77c3416c292c3ba61b9a96336fac"
+      specify "Simple"
+        $ h (Ints [0, 1, 2, 3])
+        `shouldBe` "9c6a042b7dfd0819735a839409456b303fff77c3416c292c3ba61b9a96336fac"
 
-        specify "Singleton with big int" $ do
-          h (Ints [123456789012345]) `shouldBe` "f446de5475e2f24c0a2b0cd87350927f0a2870d1bb9cbaa794e789806e4c0836"
+      specify "Singleton with big int"
+        $ h (Ints [123456789012345])
+        `shouldBe` "f446de5475e2f24c0a2b0cd87350927f0a2870d1bb9cbaa794e789806e4c0836"
 
-        specify "Simple with big ints" $ do
-          h (Ints [123456789012345, 678901234567890]) `shouldBe` "d4cca471f1c68f62fbc815b88effa7e52e79d110419a7c64c1ebb107b07f7f56"
+      specify "Simple with big ints"
+        $ h (Ints [123456789012345, 678901234567890])
+        `shouldBe` "d4cca471f1c68f62fbc815b88effa7e52e79d110419a7c64c1ebb107b07f7f56"
 
-      context "Of booleans" $ do
-        specify "Singleton true" $ do
-          h (Booleans [True]) `shouldBe` "c9757fccb1537e176de1d7fe6b3677075640873990ae195de4e2df715744ce61"
+    context "Of booleans" $ do
+      specify "Singleton true"
+        $ h (Booleans [True])
+        `shouldBe` "c9757fccb1537e176de1d7fe6b3677075640873990ae195de4e2df715744ce61"
 
-        specify "Singleton false" $ do
-          h (Booleans [False]) `shouldBe` "3f290378c94f4cc25a20b2345b418ef48628a0cdde1a7aa8a6138a3cea180f9e"
+      specify "Singleton false"
+        $ h (Booleans [False])
+        `shouldBe` "3f290378c94f4cc25a20b2345b418ef48628a0cdde1a7aa8a6138a3cea180f9e"
 
-        specify "Simple" $ do
-          h (Booleans [True, False, False]) `shouldBe` "2e27cc33925f9aae88d48ebb074dec31da423a1d379a223833af160c7f841518"
+      specify "Simple"
+        $ h (Booleans [True, False, False])
+        `shouldBe` "2e27cc33925f9aae88d48ebb074dec31da423a1d379a223833af160c7f841518"
 
-    context "Objects" $ do
-      specify "Simple" $ do
-        h (Obj1 { foo = "bar" }) `shouldBe` "7ef5237c3027d6c58100afadf37796b3d351025cf28038280147d42fdc53b960"
+  context "Objects" $ do
+    specify "Simple"
+      $ h (Obj1 { foo = "bar" })
+      `shouldBe` "7ef5237c3027d6c58100afadf37796b3d351025cf28038280147d42fdc53b960"
 
-      specify "Complex" $ do
-        h (Obj2 { foo = ["bar", "baz"], qux = ["norf"] }) `shouldBe` "f1a9389f27558538a064f3cc250f8686a0cebb85f1cab7f4d4dcc416ceda3c92"
+    specify "Complex"
+      $ h (Obj2 { foo = ["bar", "baz"], qux = ["norf"] })
+      `shouldBe` "f1a9389f27558538a064f3cc250f8686a0cebb85f1cab7f4d4dcc416ceda3c92"
